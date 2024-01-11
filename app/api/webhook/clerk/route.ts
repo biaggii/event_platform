1b1f998d721509847f5850145a5f8fc794aca913
Your Name <you@example.com>
2024-01-12 05:01:37 +0800

    fix lastname bug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -61,8 +61,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || username,
+      lastName: last_name || username,
       photo: image_url,
     }
 
